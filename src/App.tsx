66e4d8b290c243c485b475859a6fd4c3813e1ca0
Fabrizio André <fabrizio.angulo@alwa.pe>
2024-10-24 14:28:41 -0500

    Fix: Cambiar imagen de clínica

--- src/App.tsx
+++ src/App.tsx
@@ -13,33 +13,30 @@ const App: React.FC = () => {
   const isHomeRoute = location.pathname === "/";
 
   return (
-    <div
-      className={`App relative min-h-screen overflow-hidden bg-[#F8F9FF]`}
-    >
-      <Header />
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <div className="background-container relative">
-              <Home />
-              <img
-                src={blueBlur}
-                alt="Fondo difuminado azul"
-                className="absolute top-0 right-0 w-[160px] md:w-[400px] lg:w-[500px] opacity-80 pointer-events-none"
-              />
-              <img
-                src={violetBlur}
-                alt="Fondo difuminado violeta"
-                className="absolute bottom-0 left-0 w-[160px] md:w-[400px] lg:w-[500px] opacity-80 pointer-events-none"
-              />
-            </div>
-          }
-        />
-        <Route path="/planes" element={<Planes />} />
-      </Routes>
+    <div className={`App relative min-h-screen overflow-hidden bg-[#F8F9FF]`}>
+      {isHomeRoute && (
+        <>
+          <img
+            src={blueBlur}
+            alt="Fondo difuminado azul"
+            className="absolute top-0 right-0 w-[160px] md:w-[390px] pointer-events-none z-0"
+          />
+          <img
+            src={violetBlur}
+            alt="Fondo difuminado violeta"
+            className="absolute bottom-0 left-0 w-[160px] md:w-[450px] pointer-events-none z-0"
+          />
+        </>
+      )}
 
-      {isHomeRoute && <Footer />}
+      <div className="relative z-10">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/planes" element={<Planes />} />
+        </Routes>
+        {isHomeRoute && <Footer />}
+      </div>
     </div>
   );
 };
